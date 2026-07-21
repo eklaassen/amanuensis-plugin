f488e006b093e718205dd0dce9d329823a92f830
Elliott Klaassen <elliottklaassen@gmail.com>
2026-07-21 18:20:44 -0500

    chore: initialize plugin scaffold
    Add plugin.rb with metadata, engine routing, and asset registration.
    Add config/settings.yml with all 5 plugin settings (enabled, API URL,
    API secret, admin key, viewing group).

---
+++ plugin.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# name: amanuensis
+# about: Discourse plugin for viewing Writers' Room meeting records from Amanuensis
+# version: 0.1.0
+# authors: Elliott Klaassen
+# url: https://github.com/eklaassen/amanuensis
+
+register_asset 'stylesheets/amanuensis.scss'
+
+enabled_site_setting :amanuensis_enabled
+
+register_svg_icon 'clipboard-list' if respond_to?(:register_svg_icon)
+
+after_initialize do
+  module ::Amanuensis
+    PLUGIN_NAME = 'amanuensis'
+
+    class Engine < ::Rails::Engine
+      engine_name PLUGIN_NAME
+      isolate_namespace Amanuensis
+    end
+  end
+
+  require_dependency 'amanuensis/meetings_controller'
+
+  Amanuensis::Engine.routes.draw do
+    get '/' => 'meetings#index'
+    get '/meetings' => 'meetings#index'
+    get '/meetings/:id' => 'meetings#show'
+  end
+
+  Discourse::Application.routes.append do
+    mount ::Amanuensis::Engine, at: '/amanuensis'
+  end
+end
